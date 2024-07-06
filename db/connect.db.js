b0f54c6990447354eeedaf87839d22bc34cd4712
Junaid Hassan <junaid.hassan@zeikh.com>
2024-07-07 04:24:32 +0500

    cors error fixes

--- db/connect.db.js
+++ db/connect.db.js
@@ -7,7 +7,15 @@ const { dbUrl } = config;
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(
+      "mongodb+srv://gameinfodetails:testcheck09916@cluster0.icvr88i.mongodb.net/test?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 30000, // Increase timeout to 30 seconds
+        socketTimeoutMS: 30000, // Increase timeout to 30 seconds
+      }
+    );
 
     console.log("db connected");
   } catch (error) {
