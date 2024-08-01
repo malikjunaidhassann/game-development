71f3055b49ac49125060d1188dcaa4b007ac2059
Junaid Hassan <junaid.hassan@zeikh.com>
2024-08-01 15:56:48 +0500

    table fixing

--- models/table.model.js
+++ models/table.model.js
@@ -39,8 +39,46 @@ const TableSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const TournamentSchema = new mongoose.Schema(
+  {
+    tableNo: {
+      type: Number,
+      unique: true,
+    },
+    tableName: {
+      type: String,
+      required: true,
+    },
+    entryFee: {
+      type: Number,
+      required: true,
+    },
+    reward: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value >= this.entryFee;
+        },
+        message: "Reward must be greater than or equal to the entry fee.",
+      },
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    inactive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Tournament = mongoose.model("Tournament", TournamentSchema);
+
 TableSchema.plugin(autoIncrement, { inc_field: "tableNo" });
 
 const Table = mongoose.model("Table", TableSchema);
 
-export default Table;
+export { Table, Tournament };
